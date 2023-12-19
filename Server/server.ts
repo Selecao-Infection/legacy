45324a7fcf89a240e6d8ebad442889461bd083b4
AdemMlayah <adammlayah140@gmail.com>
2023-12-19 15:19:29 +0100

    done

--- Server/server.ts
+++ Server/server.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import cors from 'cors'
 import {userRoute} from './router/user_routes'
+import {questionRoute} from './router/question_routes'
 
 const app = express()
 const PORT = 4000
 app.use(cors())
 app.use(express.json())
-app.use('/api',userRoute)
+
+// user
+app.use('/user',userRoute)
+
+// question 
+app.use('/question',questionRoute)
 
 
 
