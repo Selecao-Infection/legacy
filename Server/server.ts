b6583d4665040f8cdbc96e371d5751db5be41d82
waelnasseur1 <waelnasseur6@gmail.com>
2023-12-19 16:26:37 +0100

    get ,post,update and delete product all done

--- Server/server.ts
+++ Server/server.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import cors from 'cors'
 import {userRoute} from './router/user_routes'
+import { productRoute } from "./router/product_routes";
+
 
 const app = express()
 const PORT = 4000
 app.use(cors())
 app.use(express.json())
+
+
+
 app.use('/api',userRoute)
+
+app.use('/api',productRoute)
 
 
 
