baeab563bdc6876878ee094c0304a46e5cb7e834
AdemMlayah <adammlayah140@gmail.com>
2023-12-25 15:01:19 +0100

    latest

--- Server/router/basket_routes.ts
+++ Server/router/basket_routes.ts
@@ -1,11 +1,13 @@
-import express from 'express';
-import { getAllbasket,postBasket,deleteBasket } from '../controller/basket_controller';
+    import express from 'express';
+import { getAllbasket,postBasket,deleteBasket,updateQty,decQty } from '../controller/basket_controller';
 
 export const basketRoute = express.Router();
 
-basketRoute.get('/get/basket',getAllbasket)
+basketRoute.get('/get/basket/:id',getAllbasket)
 basketRoute.post('/post/basket',postBasket)
 basketRoute.delete('/delete/basket/:id',deleteBasket)
+basketRoute.put('/up/basket',updateQty)
+basketRoute.put('/dec/basket',decQty)
 
 
 
