772693a7e3a85a41b38c1372c47ab9a09bfc622b
waelnasseur1 <waelnasseur6@gmail.com>
2023-12-26 15:22:51 +0100

    favorite page done

--- Server/router/favorite_routes.ts
+++ Server/router/favorite_routes.ts
@@ -4,6 +4,6 @@ export const favoriteRoute = express.Router();
 import { getFavorite,favoriteproduct,deleteFavorite } from "../controller/favorite_controller";
 
 
-favoriteRoute.get('/get/favorite',getFavorite)
+favoriteRoute.post('/get/favorite',getFavorite)
 favoriteRoute.post('/post/favorite',favoriteproduct)
 favoriteRoute.delete('/delete/favorite/:id',deleteFavorite)
